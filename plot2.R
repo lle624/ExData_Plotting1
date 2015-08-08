d76f36b4c7d509a7cb531e6178b0b29fc6623401
lle624 <lle624@gmail.com>
2015-08-08 14:00:14 -0400

    Code

--- plot2.R
+++ plot2.R
@@ -14,9 +14,6 @@ getData <- function(){
 
 #plot the subset of data
 plotData <- getData()
-lim <- c(0,1200)
-
-#create the png plot
 png(filename = "plot2.png", width = 480, height = 480, units = "px", bg = "white")
 plot(pData$DateTime,pData$Global_active_power,type="l", xlab="",
 ylab="Global Active Power (kilowatts)")
